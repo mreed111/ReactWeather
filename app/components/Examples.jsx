0d1b5771a0624194770d62a0459bd915d225fc16
Mike Reed <Mike Reed>
2017-05-12 20:47:44 -0700

    added foundation classes to format views

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the Examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Example locations to try:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Vallejo,CA">Vallejo,CA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Paris,FR">Paris,FR</Link>
+        </li>
+      </ol>
     </div>
   );
 };
