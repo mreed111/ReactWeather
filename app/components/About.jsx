24f8e34888d8ca777547b4fca49c23c46e230fd3
Mike Reed <Mike Reed>
2017-05-15 11:57:36 -0700

    style About page and add links to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,27 @@
 var React = require('react');
+var {Link, IndexLink} = require('react-router');
 
 // stateless functional component
 var About = (props) => {
   return (
     <div>
-      <h3>About My React Weather application:</h3>
-      <p>  This is my React Weather application.</p>
+      <h3 className="text-center">Weather application:</h3>
+      <p>  This is my React Weather application.  It will fetch the current temperature for a specified city in degrees Fahrenheit.</p>
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+      <p></p>
+      <p>
+      
+          Some resources/tools used:
+
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Used to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 };
